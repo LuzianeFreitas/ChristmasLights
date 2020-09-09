fcfb2fc60ffb0a7c9f8070fe2bac70ecdaab83f1
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-09 13:37:37 -0300

    Added functions on and off to display

---
+++ functionaties.js
@@ -0,0 +1,23 @@
+const lights = document.getElementsByClassName('lights');
+const play = document.getElementById('turn-on');
+const stop = document.getElementById('turn-off');
+
+var tam = lights.length;
+
+const on = function() {
+    for (var i = 0; i < tam; i++) {
+        lights[i].removeAttribute("style");
+        lights[i].style.animationPlayState = "running";
+        lights[i].style.WebkitAnimationPlayState = "running";
+    }
+}
+
+const off = function() {  
+    for (var i = 0; i < tam; i++) {
+        lights[i].style.animation = "none";
+        lights[i].style.background = "#62396E";
+    }
+}
+
+play.addEventListener('click', on);
+stop.addEventListener('click', off);
