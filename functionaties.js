83dbfce35a803a0d6fa425b044811335d59f271f
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-10 14:35:13 -0300

    Added function intensity

--- functionaties.js
+++ functionaties.js
@@ -1,6 +1,7 @@
 const lights = document.getElementsByClassName('lights');
 const play = document.getElementById('turn-on');
 const stop = document.getElementById('turn-off');
+const but = document.getElementById('submit');
 
 var tam = lights.length;
 
@@ -16,6 +17,13 @@ const off = function() {
     for (var i = 0; i < tam; i++) {
         lights[i].style.animation = "none";
         lights[i].style.background = "#67566E";
+    }
+}
+
+function changeIntensity() {
+    var intensity = document.getElementById('intensit').value;
+    for (let i = 0; i < tam; i++) {
+        lights[i].style.animationDuration =  intensity + "s";
     }
 }
 
